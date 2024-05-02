529279d790e954113961b5b509593e46adc7db67
Chirag Grover <97337382+JSR0100110@users.noreply.github.com>
2024-05-02 19:16:57 +0530

    Update firebase.ts

--- src/firebase/firebase.ts
+++ src/firebase/firebase.ts
@@ -7,13 +7,7 @@ import { getAnalytics } from "firebase/analytics";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyCCd-yMefQff0XQqwpfH0qjf_EOVssaQT0",
-  authDomain: "vitrack-e445e.firebaseapp.com",
-  projectId: "vitrack-e445e",
-  storageBucket: "vitrack-e445e.appspot.com",
-  messagingSenderId: "900318642725",
-  appId: "1:900318642725:web:130f149637c81d6cc075cc",
-  measurementId: "G-74XSPDJ02X"
+//information removed for security purposes
 };
 
 // Initialize Firebase
